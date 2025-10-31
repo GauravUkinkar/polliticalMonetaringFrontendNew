7f75860542d522e7e408bbf137de9ad81512afa6
Ketan Dudka <ketandudka566123@gmail.com>
2025-10-31 18:40:42 +0530

    push

---
+++ src/pages/voters_list/VotersList.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./VotersList.scss";
+const VotersList = () => {
+  return (
+    <>
+      <div class="voters_list parent">
+        <div class="voters_cont cont">
+          <div class="top">
+            <div class="prabhak_dropdown"></div>
+            <div class="search"></div>
+            <div class="btn"></div>
+          </div>
+          <div class="table"></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default VotersList;
