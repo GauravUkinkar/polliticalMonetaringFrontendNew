7cb74da335d1899ce0dfe67b317348afc7401fc6
Ketan Dudka <ketandudka566123@gmail.com>
2025-12-29 18:49:48 +0530

    push

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,8 @@ import React  from 'react'
 import './App.scss'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Dashboard from './pages/dashboard/Dashboard'
-import VotersList from './pages/voters_list/VotersList'
-import Sidebar from './comp/sidebar/Sidebar'
-import AddPrabhak from './pages/add prabhak/AddPrabhak'
+
+
 
 function App() {
  
@@ -14,11 +13,9 @@ function App() {
     <>
       <div class="app">
         <BrowserRouter>
-        <Sidebar/>
+
         <Routes>
           <Route path='/' element={<Dashboard/>} />
-          <Route path='/voterlist' element={<VotersList/>} />
-          <Route path='/addPrabhak' element={<AddPrabhak/>} />
         </Routes>
         </BrowserRouter>
       </div>
--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,35 +1,13 @@
-import React, { useEffect, useState } from "react";
 import "./Dashboard.scss";
-import { getAllvillage } from "../../api/Api";
-import { Link } from "react-router-dom";
-const Dashboard = () => {
-  const [lengthData, setLengthData] = useState({
-    villageCount: 0,
-  });
 
-  useEffect(() => {
-    getAllvillage()
-      .then((res) => {
-        setLengthData({
-          ...lengthData,
-          villageCount: res.length,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
+const Dashboard = () => {
+
+
+ 
 
   return (
     <>
-      <div class="dashboard_parent parent">
-        <Link to={`/voterlist?query=village`} class="tab">
-          गाव नुसार <span>{lengthData?.villageCount}</span>{" "}
-        </Link>
-        <div class="tab">गणा नुसार</div>
-        <div class="tab">आडनाव नुसार</div>
-        <div class="tab">प्रभाक नुसार</div>
-      </div>
+      
     </>
   );
 };
